810c7a5bfe35290777ab865479c2edbb1122988a
Alif Laksono <85070659+alif7laksono@users.noreply.github.com>
2025-09-29 17:56:44 +0700

    add env to vercel

--- app/admin/suggestions/page.tsx
+++ app/admin/suggestions/page.tsx
@@ -9,7 +9,7 @@ export default async function SuggestionsPage() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect("/login"); // redirect kalau belum login
+    redirect("/login");
   }
 
   return (
