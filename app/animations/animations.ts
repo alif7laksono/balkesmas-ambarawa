4d8bee8ffd414d05077354570b63a03c36d04fa4
Alif Laksono <85070659+alif7laksono@users.noreply.github.com>
2025-10-03 11:08:55 +0700

    update animations.ts

--- app/animations/animations.ts
+++ app/animations/animations.ts
@@ -58,3 +58,14 @@ export const container = {
     transition: { staggerChildren: 0.12 },
   },
 };
+
+export const footerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.3,
+    },
+  },
+};
